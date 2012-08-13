2a47561f34b61c240a5c34ec2671b09df2a387f5
Leigh Dodds <leigh@ldodds.com>
2012-08-13 10:39:45 +0100

    Fixed crawler after changes to LR website

--- bin/price-paid.rb
+++ bin/price-paid.rb
@@ -3,7 +3,7 @@ require 'csv'
 require 'hpricot'
 require 'open-uri'
 
-page = Hpricot( open("http://www1.landregistry.gov.uk/market-trend-data/price-paid-data/price-paid-data-files") )
+page = Hpricot( open("http://www.landregistry.gov.uk/public/information/public-data/price-paid-data/price-paid-data-files") )
 
 dir = File.dirname(__FILE__) 
 
@@ -12,20 +12,21 @@ if !File.exists?( File.join(dir, "..", "data") )
 end
 
 HEADERS = [
-  "Unique ID","Price date", "Postcode","Property type old/new", "Duration", "PAON", 
+  "Unique ID","Price","date", "Postcode","Property type", "old/new", "Duration", "PAON", 
   "SAON","Street","Locality","Town","District","County","Record Status"]
 
 CSV.open( File.join(dir, "..", "data/bath-house-prices.csv"), "w") do |output|
   output << HEADERS
-  page.search("#middle a").each do |link|
-    if link["href"].match(/csv$/)
-      $stderr.puts("Processing #{link["title"]}")
-      data = open("http://www1.landregistry.gov.uk#{link["href"]}").read
-      CSV.parse( data ) do |row|
-        if row[3] != nil && row[3].match(/^BA(1|2) /)
-          output << row
-        end       
-      end
+  page.search("table tr")[1..-1].reverse_each do |row| 
+    $stderr.puts("Processing #{row.at("td").inner_text}")
+    #Extract download link for CSV without column headers
+    url = row.search("td")[2].at("a")["href"]
+    data = open(url).read
+    CSV.parse( data ) do |row|
+         if row[3] != nil && row[3].match(/^BA(1|2) /)
+           output << row
+         end       
     end
   end
+
 end
