86b0c1c79b834993b48c2fd7d5666992c3905275
Leigh Dodds <leigh@ldodds.com>
2013-06-18 09:35:57 +0100

    Use newer page with full list of data; improve scraping

--- bin/price-paid.rb
+++ bin/price-paid.rb
@@ -3,7 +3,7 @@ require 'csv'
 require 'hpricot'
 require 'open-uri'
 
-page = Hpricot( open("http://www.landregistry.gov.uk/public/information/public-data/price-paid-data/price-paid-data-files") )
+page = Hpricot( open("http://www.landregistry.gov.uk/market-trend-data/public-data/price-paid-data") )
 
 dir = File.dirname(__FILE__) 
 
@@ -17,15 +17,17 @@ HEADERS = [
 
 CSV.open( File.join(dir, "..", "data/bath-house-prices.csv"), "w") do |output|
   output << HEADERS
-  page.search("table tr")[1..-1].reverse_each do |row| 
-    $stderr.puts("Processing #{row.at("td").inner_text}")
-    #Extract download link for CSV without column headers
-    url = row.search("td")[2].at("a")["href"]
-    data = open(url).read
-    CSV.parse( data ) do |row|
-         if row[3] != nil && row[3].match(/^BA(1|2) /)
-           output << row
-         end       
+  page.search("#c-content .datatable tbody tr").reverse_each do |row| 
+    if row.children
+        $stderr.puts("Processing #{row.at("td").inner_text}")
+        #Extract download link for CSV without column headers
+        url = row.search("td")[2].at("a")["href"]
+        data = open(url).read
+        CSV.parse( data ) do |row|
+             if row[3] != nil && row[3].match(/^BA(1|2) /)
+               output << row
+             end       
+        end
     end
   end
 
